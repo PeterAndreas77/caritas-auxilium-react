c66bc119b38dec7865cbc6f9163e5e3c05138f2c
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-09-28 10:48:10 -0700

    fixed connection

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_URL: "http://localhost:8000" || "https://caritas-auxilium.herokuapp.com"
+  API_URL: "https://caritas-auxilium.herokuapp.com",
+  API_LOCAL: "http://localhost:8000"
 };
