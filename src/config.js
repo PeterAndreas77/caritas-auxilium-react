f8be6d660b7ba0d7d30593863e0e6fa6b651b279
PeterAndreas77 <andreas.soedharto@gmail.com>
2018-09-28 11:10:14 -0700

    fixed url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_URL: "https://caritas-auxilium.herokuapp.com",
-  API_LOCAL: "http://localhost:8000"
+  API_URL:
+    process.env.REACT_APP_BASE_URL || "https://caritas-auxilium.herokuapp.com",
+  API_LOCAL: process.env.REACT_APP_BASE_URL || "http://localhost:8000"
 };
